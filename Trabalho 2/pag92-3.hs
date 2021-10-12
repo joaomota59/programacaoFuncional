87f003e9bf7e66d3accb5abf6f0050f5bf183cb4
João Lucas Mota <joaomota59@hotmail.com>
2021-10-12 10:39:41 -0300

    finalizando questao 3 do trabalho 2

---
+++ Trabalho 2/pag92-3.hs
@@ -0,0 +1,17 @@
+{-
+Uma função que informa se um determinado livro está ou não emprestado
+-}
+
+type Pessoa = String
+type Livro = String
+type BancodeDados = [(Pessoa, Livro)]
+
+db :: BancodeDados
+db = [("Paulo", "A Mente Nova do Rei"), ("Ana", "O Segredo de Luiza"),
+ ("Paulo", "O Pequeno Principe"),("Mauro", "O Capital"),
+ ("Francisco", "O Auto da Compadecida"),("Pedro", "A Mente Nova do Rei")]
+
+livroEmprestado :: Livro -> BancodeDados -> Bool
+livroEmprestado livroParametro [] = False
+livroEmprestado livroParametro ((pessoa,livro):resto) = livro==livroParametro || livroEmprestado livroParametro resto
+
