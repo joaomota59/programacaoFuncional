ea532206e66b1cffdf6ad859d8d71f93ca3302cb
João Lucas Mota <joaomota59@hotmail.com>
2021-10-13 10:24:11 -0300

    otimizando funcao

--- Trabalho 2/pag92-2.hs
+++ Trabalho 2/pag92-2.hs
@@ -1,6 +1,7 @@
 {-
 Uma funcao que informa todas as pessoas que tomaram emprestado um determinado
 livro.
+base 
 -}
 
 type Pessoa = String
@@ -12,15 +13,9 @@ db = [("Paulo", "A Mente Nova do Rei"), ("Ana", "O Segredo de Luiza"),
  ("Paulo", "O Pequeno Principe"),("Mauro", "O Capital"),
  ("Francisco", "O Auto da Compadecida"),("Pedro", "A Mente Nova do Rei")]
 
-
-livroPertence::Livro -> Livro -> Bool --verifica o nome do livro passado é igual a o livro da tupla
-livroPertence a b = a == b
-
 pessoasPorLivroString :: Livro -> BancodeDados -> String
 pessoasPorLivroString livroParametro [] = ""
-pessoasPorLivroString livroParametro ((pessoa,livro):resto) =  (if livroPertence livroParametro livro then pessoa++"\n" else "")++pessoasPorLivroString livroParametro resto
-
-
+pessoasPorLivroString livroParametro ((pessoa,livro):resto) =  (if livroParametro == livro then pessoa++"\n" else "")++pessoasPorLivroString livroParametro resto
 
 pessoasPorLivro :: Livro -> BancodeDados -> IO() -- parametros -> (nomeDoLivro, database)
 pessoasPorLivro livroParametro ((pessoa,livro):resto) =  putStr (pessoasPorLivroString livroParametro ((pessoa,livro):resto))
