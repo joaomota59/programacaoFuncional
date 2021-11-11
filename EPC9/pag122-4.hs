a1f94b5273742591baefa3c8ba189749cf1b627b
João Lucas Mota <joaomota59@hotmail.com>
2021-11-11 18:25:33 -0300

    finalizando questao 4

--- EPC9/pag122-4.hs
+++ EPC9/pag122-4.hs
@@ -6,8 +6,13 @@ ocorrem exatamente uma vez em uma lista. Por exemplo, ocorreUmaV ez[2; 4; 2; 1; 
 numOcorre :: (Eq t) => [t] -> t -> Int
 numOcorre lista item = sum [1 | i <-lista , item == i]
 
-conjuntoL :: [Int] -> [Int]
-
-conjuntoL (a:x) listaAux = a :
+conjuntoL :: [Int] -> [Int] -> [Int]
+conjuntoL [] _ = []
+conjuntoL (a:x) listaAux
+    |(a `elem` listaAux) = conjuntoL x listaAux
+    |otherwise = a : conjuntoL x (a:listaAux) 
 
 ocorreUmaVez :: [Int] -> [Int]
+ocorreUmaVez lista = 
+    let result = (conjuntoL lista [])
+    in [i | i <- result, (numOcorre lista i) == 1]
