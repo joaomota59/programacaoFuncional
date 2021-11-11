6f6b0f5f723e24c547c212937c577dd551654354
João Lucas Mota <joaomota59@hotmail.com>
2021-11-11 19:00:05 -0300

    finalizando epcs

--- EPC9/pag122-1.hs
+++ EPC9/pag122-1.hs
@@ -1,3 +1,7 @@
+{-
+Mostre todos os passos realizados na chamada iSort[2, 8, 1].
+-}
+
 ins :: Int -> [Int] -> [Int]
 ins a [ ] = [a]
 ins a (b:y)
@@ -8,19 +12,64 @@ iSort :: [Int] -> [Int]
 iSort [ ] = [ ]
 iSort (a:x) = ins a (iSort x)
 
-main :: IO ()
-main =  do
-putStr "Sum of x + y = "
-print(iSort [2,8,1])
-
 {-
-iSort chama ins passa a=2 e x = [8,1] para ins
-a = 2
-b = 8
-y = [1]
-a <= b, True, entao lista = [2] : ins [8,1]
-a = 8
-b = 1
-a<=b, False, entao  [2]:[1]:[]
+iSort [2,8,1]
+a = 2 e x = [8,1]
+começar de dentro para fora:
+
+ins 2 (iSort (ins 8 (isort (ins 1 iSort []))) )
+
+parte mais interna:
+    a = 1 e lista vazia, entao cai na primeira condicao a [] = [a],
+    ins 1 [] = [1]
+entao fica:
+    ins 2 (isort (ins 8 (isort [1] )))
+
+parte mais interna: 
+    ins 8 (isort [1])
+    a = 8
+    b = 1
+    y = []
+    8 <= 1, False, entao,
+    1 : ins 8 [],
+    ins 8 [] = [8], entao,
+    1:[8] = [1,8]
+
+entao fica:
+    ins 2 (isort ([1,8]))
+parte mais interna:
+    isort [1,8]
+    a = 1
+    x = [8]
+    isort [1,8] = ins 1 iSort [8]
+parte mais interna:
+    iSort [8]
+    a = 8
+    x = []
+    iSort [8] = ins 8 [] = [8]
+voltando para a linha 44: ins 1 [8]
+    a = 1
+    b = 8
+    1<=8, True, entao
+    ins 1 [8] = 1: 8 : []
+    isort = ins 1 [8] = [1,8]
+parte mais externa:
+    ins 2 (isort ([1,8])) = ins 2 [1,8]
+    a = 2
+    b = 1
+    y = [8]
+    2<=1, False, entao
+    1 : ins 2 [8]
+    ins 2 [8]
+    a = 2
+    b = 8
+    y = []
+    2<=8, True,entao
+    ins 2 [8] = 2 : 8: [] = [2,8]
+    concatenando o resultado com 1 : ins 2 [8]
+    1 : [2,8] = [1,2,8]
+
+Resultado = [1,2,8]
+
 
 -}
--- EPC9/pag122-4.hs
+++ EPC9/pag122-4.hs
@@ -1,6 +1,6 @@
 {-
 Defina a funcao ocorreUmaVez :: [Int] -> [Int] que retorna a lista de numeros que
-ocorrem exatamente uma vez em uma lista. Por exemplo, ocorreUmaV ez[2; 4; 2; 1; 4] = [1].
+ocorrem exatamente uma vez em uma lista. Por exemplo, ocorreUmaVez[2, 4, 2, 1, 4] = [1].
 -}
 
 numOcorre :: (Eq t) => [t] -> t -> Int
